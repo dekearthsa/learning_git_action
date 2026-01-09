8ee3a4ec54741f067f15feabc6ff88dc335eb840
pcsishun <dekearthsa@hotmail.com>
2026-01-09 22:37:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import axios from axios;
+
+export const handler = async (event) => {
+    const raw = axios.get("https://c55bf3ed7453.ngrok-free.app");
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(raw.data),
+    };
+    return response;
+};
